77c85ed477d94f5bfc9d636a99d3a779af6610e7
ansari-sahil <sahilansari20031010@gmail.com>
2024-07-31 18:59:29 +0530

    cleaning

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,20 +5,20 @@ import Weather from "./components/Weather";
 const apiKey = "f28d0c48fe92640ea8341d10c62bb170";
 
 export default function App() {
-  const [weather, setWeather] = useState(null); // Initial state set to null
-  const [loading, setLoading] = useState(false); // State to handle loading
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchWeather = async (city) => {
-    setLoading(true); // Start loading
+    setLoading(true); 
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
       const response = await fetch(url);
       const data = await response.json();
       setWeather(data);
     } catch (error) {
-      setWeather(null); // Clear weather data on error
+      setWeather(null);
     } finally {
-      setLoading(false); // Stop loading
+      setLoading(false); 
     }
   };
 
